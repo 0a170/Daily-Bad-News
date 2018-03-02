423b46c8c0cd3cbf46336c3f239e01f234d986aa
mahussensr <mahussensr@gmail.com>
2018-03-01 18:22:05 -0600

    updated page styling

--- public/js/main.js
+++ public/js/main.js
@@ -8,11 +8,12 @@ $(document).ready(function() {
          success: function(response) {
             var newsArticles = response;
             for(let i = 0; i < newsArticles.length; i++) {
-               content += '<div class="col-sm-3 article-div">'
-                          + '<img src="' + newsArticles[i].urlToImage + '" style="width: 100%">'
-                          + '<p><b>' + newsArticles[i].title + '</b></p><br>'
-                          + '<p>' + newsArticles[i].description + '</p><br>'
-                          + '</div>';
+               content += '<div class="col-sm-4 article-div">'
+                       +     '<img src="' + newsArticles[i].urlToImage + '" style="width: 100%">'
+                       +     '<p><b>' + newsArticles[i].title + '</b></p>'
+                       +     '<div class="divider"></div>'
+                       +     '<p>' + newsArticles[i].description + '</p>'
+                       +  '</div>';
             }
             $('#content').html(content);
          },
